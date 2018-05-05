d3dcbd15b7f30650211411e9a53d7d30ca7c7b7a
Kevin E. Rafferty II <kevineraffertyii@kevins-macbook-pro.local>
2018-05-04 19:07:10 -0700

    KERII - Initial commit. * Basic Xcode Project setup.

---
+++ WagCodingChallenge/WagCodingChallenge/Application Delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WagCodingChallenge
+//
+//  Created by Kevin E. Rafferty II on 5/4/18.
+//  Copyright © 2018 krafferty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
