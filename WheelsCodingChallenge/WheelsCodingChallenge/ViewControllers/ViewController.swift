c116647019226760ed1de39e3520f95b56fa3f83
Kevin E. Rafferty II <kevin.rafferty@gaidohealth.com>
2019-09-20 20:09:47 -0700

    KERII - Added a new Networking API class.     - Fleshed out the GET Stack Overflow Users method. * Added the User model class that conforms to the Decodable protocol.     - Fleshed out the User model class to contain all of the coding keys needed to parse the Stack Overflow JSON.

--- WheelsCodingChallenge/WheelsCodingChallenge/ViewControllers/ViewController.swift
+++ WheelsCodingChallenge/WheelsCodingChallenge/ViewControllers/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
