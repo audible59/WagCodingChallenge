61370c6d6324cdbe49db33d9e50e3f5d035e1ed3
Kevin E. Rafferty II <kevin.rafferty@gaidohealth.com>
2019-09-20 17:48:51 -0700

    KERII - Initial Project creation.

---
+++ WheelsCodingChallenge/WheelsCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheelsCodingChallenge
+//
+//  Created by Kevin E. Rafferty II on 9/20/19.
+//  Copyright © 2019 Kevin E. Rafferty II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
