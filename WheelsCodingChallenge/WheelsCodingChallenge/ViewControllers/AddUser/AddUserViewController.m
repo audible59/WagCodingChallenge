aa31a6a27e22c3f3d70f69a758ddca20a882c5e9
Kevin E. Rafferty II <kevin.rafferty@gaidohealth.com>
2019-09-21 19:08:06 -0700

    KERII - Added a new UIViewController for the Add User screen. * Added the necessary Bridging Header to support both Swift and Objective-C.

---
+++ WheelsCodingChallenge/WheelsCodingChallenge/ViewControllers/AddUser/AddUserViewController.m
@@ -0,0 +1,33 @@
+//
+//  AddUserViewController.m
+//  WheelsCodingChallenge
+//
+//  Created by Kevin E. Rafferty II on 9/21/19.
+//  Copyright © 2019 Kevin E. Rafferty II. All rights reserved.
+//
+
+#import "AddUserViewController.h"
+
+@interface AddUserViewController ()
+
+@end
+
+@implementation AddUserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
